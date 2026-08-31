bd83137ab963fc880a1eaa60ab17bf9805eda296
İlaha <ilaha9668@gmail.com>
2026-09-01 01:32:43 +0400

    Update api.js
    connect live backend

--- src/components/data/api.js
+++ src/components/data/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 async function getProduct() {
-    const res = await axios.get("http://localhost:3000/api/products");
+    const res = await axios.get("https://api-vercel-master.onrender.com");
     // JSON-da id yoxdur, ona görə sıra nömrəsini id kimi veririk
     return res.data.map((product, index) => ({ id: index + 1, ...product }));
 }
